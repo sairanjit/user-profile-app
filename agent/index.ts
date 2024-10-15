f1fe6e27945ee0ef3fcf723c6160b4958f708537
Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
2024-10-15 10:30:48 +0530

    feat: add credo-ts
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>

---
+++ agent/index.ts
@@ -0,0 +1,2 @@
+export * from "./agent"
+export * from "./requestPermissions"
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,94 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import "react-native-get-random-values"
+import "fast-text-encoding"
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native"
+import { useFonts } from "expo-font"
+import { Stack } from "expo-router"
+import * as SplashScreen from "expo-splash-screen"
+import { useEffect, useState } from "react"
+import "react-native-reanimated"
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useColorScheme } from "@/hooks/useColorScheme"
+import type { InitConfig } from "@credo-ts/core"
+import {
+  Agent,
+  ConnectionsModule,
+  ConsoleLogger,
+  LogLevel,
+} from "@credo-ts/core"
+import { AskarModule } from "@credo-ts/askar"
+import { agentDependencies } from "@credo-ts/react-native"
+import AgentProvider from "@credo-ts/react-hooks"
+import { View } from "react-native"
+import { ariesAskar } from "@hyperledger/aries-askar-react-native"
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const [agent, setAgent] = useState()
+  const colorScheme = useColorScheme()
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  })
+
+  // Initialize agent
+  useEffect(() => {
+    if (agent) return
+
+    const startAgent = async () => {
+      try {
+        const config: InitConfig = {
+          label: "Kevin",
+          logger: new ConsoleLogger(LogLevel.debug),
+          walletConfig: {
+            id: "wallet-id",
+            key: "secure-key",
+          },
+        }
+
+        const newAgent = new Agent({
+          config,
+          dependencies: agentDependencies,
+          modules: {
+            askar: new AskarModule({ ariesAskar }),
+            connections: new ConnectionsModule({ autoAcceptConnections: true }),
+          },
+        })
+
+        await newAgent.initialize()
+        if (!newAgent) return
+
+        setAgent(newAgent)
+      } catch (error) {
+        console.log("Error initializing agent", error)
+      }
+    }
+
+    void startAgent()
+  }, [])
 
   useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
+    if (loaded && agent) {
+      SplashScreen.hideAsync()
     }
-  }, [loaded]);
+  }, [loaded, agent])
 
-  if (!loaded) {
-    return null;
+  if (!loaded || !agent) {
+    return null
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    // <AgentProvider agent={agent}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
     </ThemeProvider>
-  );
+    // </AgentProvider>
+  )
 }
